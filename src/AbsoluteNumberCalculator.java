87ffc34d53cb8cc3c7e503104aa4b2f0cf1899e6
Nguyen Long <nguyenlong232253@gmail.com>
2022-03-31 10:20:32 +0700

    nopbaitap

---
+++ src/AbsoluteNumberCalculator.java
@@ -0,0 +1,10 @@
+public class AbsoluteNumberCalculator {
+    //    public static int finAbsolute(int number){
+//        return 0;
+//    }
+    public static int findAbsolute(int number) {
+        if (number < 0)
+            return -number;
+        return number;
+    }
+}
